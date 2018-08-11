64349c7c18fc30fe413b1c98b68e442f06149d10
diwasjoshi <diwas.joshi2@gmail.com>
2018-08-11 08:15:46 +0000

    bug fix

--- redisClient.js
+++ redisClient.js
@@ -1,6 +1,6 @@
 const keys = require('./keys.js');
 var redis = require('redis');
-var redisClient = redis.createClient({port: keys.REDIS_PORT, host: keys.REDIS_HOST, password: REDIS_PASS});
+var redisClient = redis.createClient({port: keys.REDIS_PORT, host: keys.REDIS_HOST, password: keys.REDIS_PASS});
 redisClient.config("SET", "notify-keyspace-events", "KExe");
 
 /*var RedisNotifier = require('redis-notifier');
