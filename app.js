2fc46d7b4270aa3fa71779df267eaf821f443354
diwasjoshi <diwas.joshi2@gmail.com>
2018-08-11 12:12:18 +0000

    constant cors host

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ counter.addCount();
 
 //Enable CORS
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', keys.CLIENT_HOST_TO_ALLOW);
+    res.setHeader('Access-Control-Allow-Origin', "https://create-shorturl.herokuapp.com");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
